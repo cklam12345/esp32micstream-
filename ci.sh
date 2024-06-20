aa766f7aae71bd596f198d081bc6c1907499e9e6
cklam12345 <cklam12345@gmail.com>
2024-06-20 11:02:52 -0700

    first commit

---
+++ ci.sh
@@ -0,0 +1,7 @@
+echo "# esp32micstream-" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:cklam12345/esp32micstream-.git
+git push -u origin main
